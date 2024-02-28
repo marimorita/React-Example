84b0959974b95811e6352bb489ae1ad9f7ad4a1e
123juanda <junieto.975@gmail.com>
2024-02-28 17:40:49 -0500

    Finish header

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,3 +1,4 @@
 import React from 'react'
+import './Logo.css'
 
 export const Logo = ({content}) => <img src={content} alt="logo" />
--- src/components/Pages/Perfil/Perfil.jsx
+++ src/components/Pages/Perfil/Perfil.jsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import "./Perfil.css"
+import sena from '../../../assets/sena.png';
+import { Logo } from '../../Logo/Logo';
 
 export const Perfil = () => {
   return (
-    <div>Perfil</div>
+    <>
+    <section className='description'>
+      <Logo content={sena} />
+     <h1>PERFIL</h1>
+     <hr></hr>
+     <p>El Lorem Ipsum fue concebido como un texto de relleno, formateado de una cierta
+     manera para permitir la presentación de elementos gráficos en documentos, sin
+     necesidad de una copia formal. El uso de Lorem Ipsum permite a los diseñadores reunir
+     los diseños y la forma del contenido antes de que el contenido se haya creado, dando
+     al diseño y al proceso de producción más libertad.</p>
+     </section>
+    </>
   )
 }
