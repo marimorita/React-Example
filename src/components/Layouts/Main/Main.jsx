82cd83aad57242729379613841c7b6140d423b45
marimorita <marimm0927@gmail.com>
2024-02-29 16:04:03 -0500

    confirm second

--- src/components/Layouts/Main/Main.jsx
+++ src/components/Layouts/Main/Main.jsx
@@ -5,8 +5,6 @@ export const Main = ({children}) => {
   return (
     <main>{children}</main>
   )
-<<<<<<< HEAD
+
 }
-=======
-}
->>>>>>> 84b0959974b95811e6352bb489ae1ad9f7ad4a1e
+
--- src/components/Pages/Perfil/Perfil.jsx
+++ src/components/Pages/Perfil/Perfil.jsx
@@ -7,15 +7,15 @@ export const Perfil = () => {
   return (
     <>
     <section className='description'>
-<<<<<<< HEAD
+
       <div className='profileImage'>
         <img src={sena} alt="Perfil" className="perfil-image" />
         <h1>PERFIL</h1>
       </div>
-=======
+
       <Logo content={sena} />
      <h1>PERFIL</h1>
->>>>>>> 84b0959974b95811e6352bb489ae1ad9f7ad4a1e
+
      <hr></hr>
      <p>El Lorem Ipsum fue concebido como un texto de relleno, formateado de una cierta
      manera para permitir la presentación de elementos gráficos en documentos, sin
